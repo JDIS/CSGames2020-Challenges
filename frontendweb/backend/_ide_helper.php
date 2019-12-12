1559f8815292dd5427cc46595849e65a78c50c05
Émilio Gonzalez <egg997@gmail.com>
2019-12-12 09:39:04 -0800

    backend pomal done

--- frontendweb/backend/_ide_helper.php
+++ frontendweb/backend/_ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.7.0 on 2019-12-12 15:24:38.
+ * Generated for Laravel 6.7.0 on 2019-12-12 17:30:28.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
--- frontendweb/backend/app/Http/Controllers/MyController.php
+++ frontendweb/backend/app/Http/Controllers/MyController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Incident;
 use Illuminate\Http\Request;
 
 class MyController extends Controller
 {
     public function getData(Request $request) {
-        return ['data' => 5];
+        $n = 15;
+        if($request['n']) {
+            $n = $request['n'];
+        }
+        $incidents = Incident::paginate($n);
+        return \Response::json($incidents);
     }
 }
