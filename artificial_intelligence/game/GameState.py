692f66d8b5cc760886aa101bb9240b5b147333b9
AntoineTheb <theberge.antoine.cem@gmail.com>
2020-01-01 15:25:59 -0500

    Added documentation

--- artificial_intelligence/game/GameState.py
+++ artificial_intelligence/game/GameState.py
@@ -1,36 +1,54 @@
 class GameState():
-    # TODO: Doc
+    '''
+    State representing the tic tac toe board
+    '''
 
     def __init__(self):
-        # TODO: Doc
+        '''
+        Board that lists the positions
+        '''
+
+        # Board with no tokens placed
         self.board = list('123456789')
+        # Win conditions
         self.wins = ((0, 1, 2), (3, 4, 5), (6, 7, 8),
                      (0, 3, 6), (1, 4, 7), (2, 5, 8),
                      (0, 4, 8), (2, 4, 6))
 
     def _space(self):
-        # TODO: Doc
+        '''
+        Available positions
+        '''
+
         return [b for b in self.board if b not in 'XO']
 
     def get_valid_actions(self):
-        # TODO: Doc
+        '''
+        Return available positions
+        '''
+
         return self._space()
 
-    def apply_action(self, action, player):
-        # TODO: Doc
+    def apply_action(self, action: int, player: str):
+        '''
+        Generate a new state and apply the action to it
+        '''
+
         new_state = self.get_copy()
         new_state.board[int(action)-1] = player
         return new_state
 
     def is_terminal(self):
-        # TODO: Doc
+        '''
+        Return whether the game is over or not, and who has won
+        '''
 
         # A player has won
         for w in self.wins:
             b = self.board[w[0]]
             if b in 'XO' and all(self.board[i] == b for i in w):
                 return (True, -1) if b == 'X' else (True, 1)
-        # A tie has happened, -2 so it's in favor of Player 2
+        # A tie has happened
         if all(b in 'XO' for b in self.board):
             return (True, 0)
         # State is not terminal
@@ -38,22 +56,37 @@ class GameState():
             return (False, 0)
 
     def get_copy(self):
-        # TODO: Doc
+        '''
+        Get a copy of the state
+        '''
+
         return GameState.from_string(self.to_string()[:])
 
     @staticmethod
-    def from_string(string):
+    def from_string(board: str):
+        '''
+        Generate a new state based on a
+        string representation of the board
+        '''
+
         game = GameState()
-        for c in string:
+        for c in board:
             if c not in '123456789XO':
                 raise AssertionError('Invalid state')
-        game.board = list(string[:])
+        game.board = list(board[:])
         return game
 
     def to_string(self):
+        '''
+        Generate a string representation of the board
+        '''
+
         str_state = ''
         return str_state.join(self.board)
 
     def printable_board(self):
-        # TODO: Doc
+        '''
+        Return a human-readable version of the board
+        '''
+
         return '\n'.join(' '.join(self.board[x:x+3]) for x in (0, 3, 6))
--- artificial_intelligence/server/Client.py
+++ artificial_intelligence/server/Client.py
@@ -5,29 +5,41 @@ import socket
 from game.GameState import GameState
 
 HOST = '0.0.0.0'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+PORT = 65432      # The port used by the server
 
 
+# Establish connection
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
-    # TODO: Comments
+    # Connect to host
     s.connect((HOST, PORT))
+
+    # Must play 50 game
     for _ in range(50):
         while True:
-            i = 0
+            # Receive data from server
             data = s.recv(1024)
+            # Parse data
             state_str = data.decode('utf-8').replace('\n', '').replace(' ', '')
+            # If the data received is a state, build the state
             try:
                 state = GameState.from_string(state_str)
-            # Cannot parse state, CPU has returned something else
+            # Cannot parse state, Server has returned something else
             except AssertionError:
                 print(state_str)
                 break
+            # Print the board so it's human-readable
             print(state.printable_board())
+            # Get the valid options from the
+            # state to prevent returning an invalid action
             options = state.get_valid_actions()
+            # Choose an action at random
+            # TODO: Return a good action
             choice = random.choice(options)
             print('I play', choice)
+            # Send the action to the server
             s.sendall(bytes(choice, encoding='utf-8'))
+        print('---------------------------')
 
-    data = s.recv(1024)
-    print(data.decode('utf-8'))
+    # Receive the flag
+    print('\n', s.recv(1024).decode('utf-8'))
