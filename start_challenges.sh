9f595457fbcf98bf2ac8c052f8da3b42439cc3e2
Emile Fugulin <emilefugulin@hotmail.com>
2020-01-23 17:18:16 -0500

    Update deploy script

--- start_challenges.sh
+++ start_challenges.sh
@@ -1,1 +1,7 @@
 docker-compose -f frontendweb/backend/docker-compose.yml up -d
+docker-compose -f theory/numbers/numbers_server/docker-compose.yml up -d
+docker-compose -f artificial_intelligence/Tic-T-AI-Toe/tictaitoe_server/docker-compose.yml up -d
+docker-compose -f crypto/rsa_1/rsa_1_server/docker-compose.yml up -d
+docker-compose -f crypto/rsa_2/rsa_2_server/docker-compose.yml up -d
+docker-compose -f crypto/rsa_3/rsa_3_server/docker-compose.yml up -d
+docker-compose -f relay-tracing/relay_tracing_server/docker-compose.yml up -d
